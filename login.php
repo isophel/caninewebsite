1bcf635a845c5301c8436f7e969ac2aa4f3e9fce
Natwijuka Isophel <natisox18@gmail.com>
2021-08-23 21:39:20 +0300

    added login and registration page

--- login.php
+++ login.php
@@ -1,5 +1,22 @@
 <?php require_once('config.php');?>
 <?php require_once(ROOT_PATH . '/includes/header.php'); ?>
 <?php  require_once(ROOT_PATH . '/includes/registration_login.php'); ?>
+<div class="container">
+	
+
+	<div style="width: 40%; margin: 20px auto;">
+		<form method="post" action="login.php" >
+			<h2>Login</h2>
+			<?php include(ROOT_PATH . '/includes/errors.php') ?>
+			<input type="text" name="username" value="<?php echo $username; ?>" value="" placeholder="Username">
+			<input type="password" name="password" placeholder="Password">
+			<button type="submit" class="btn" name="login_btn">Login</button>
+			<p>
+				Not yet a member? <a href="register.php">Sign up</a>
+			</p>
+		</form>
+	</div>
+</div>
+<!-- // container -->
 
 <?php require_once(ROOT_PATH . '/includes/footer.php'); ?>
