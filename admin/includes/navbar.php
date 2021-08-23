390fc574ebe517f0c10de096cd56baae0c1b6b30
Natwijuka Isophel <natisox18@gmail.com>
2021-08-24 00:24:12 +0300

    creating post in the admin

--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -5,6 +5,6 @@
 		</a>
 	</div>
 	<div class="user-info">
-		<span>Isophel</span> &nbsp; &nbsp; <a href="<?php echo BASE_URL . '/logout.php'; ?>" class="logout-btn">logout</a>
+		<span><? php echo user['username'];?></span> &nbsp; &nbsp; <a href="<?php echo BASE_URL . '/logout.php'; ?>" class="logout-btn">logout</a>
 	</div>
 </div>
