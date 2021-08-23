a8ffd046232391751e1e5619c1625ae72e31a5fd
Natwijuka Isophel <natisox18@gmail.com>
2021-08-23 22:57:57 +0300

    updating the admin

--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -1,10 +1,10 @@
 <div class="header">
 	<div class="logo">
 		<a href="<?php echo BASE_URL .'admin/dashboard.php' ?>">
-			<h1>LifeBlog - Admin</h1>
+			<h1>Canine Safaris - Admin</h1>
 		</a>
 	</div>
 	<div class="user-info">
-		<span>Awa</span> &nbsp; &nbsp; <a href="<?php echo BASE_URL . '/logout.php'; ?>" class="logout-btn">logout</a>
+		<span>Isophel</span> &nbsp; &nbsp; <a href="<?php echo BASE_URL . '/logout.php'; ?>" class="logout-btn">logout</a>
 	</div>
 </div>
